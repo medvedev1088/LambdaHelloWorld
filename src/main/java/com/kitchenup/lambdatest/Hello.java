b25f8bfcd173ec8c1592291e3a9eb401d65024a3
Evgeny Medvedev <evgeny@kitchenup.com>
2016-01-26 15:43:47 +0700

    Initial commit

---
+++ src/main/java/com/kitchenup/lambdatest/Hello.java
@@ -0,0 +1,12 @@
+package com.kitchenup.lambdatest;
+
+import com.amazonaws.services.lambda.runtime.Context; 
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class Hello {
+    public String myHandler(int myCount, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("received : " + myCount);
+        return String.valueOf(myCount);
+    }
+}
